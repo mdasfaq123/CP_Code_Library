165f21bd547ac900f24c860e1f69ca670727e3d8
mdasfaq123 <mdasfaqahbabmayaz@gmail.com>
2026-01-01 15:08:13 +0600

    Add files via upload

---
+++ Difference_Array.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+int32_t main()
+{
+	ios_base :: sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n; cin >> n;
+	vector < int > dif(n+2);
+	int q; cin >> q;
+	while(q--)
+	{
+		int l, r; cin >> l >> r;
+		dif[l]++;
+		dif[r+1]--;
+	}
+	for(int i = 1; i <= n; i++)
+	{
+		dif[i] += dif[i-1];
+		cout << dif[i] << " ";
+	}
+	cout << endl;
+	return 0;
+}
